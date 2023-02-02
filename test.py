62e2066d16c0271289ef849ed52f91538ea89a44
urez06 <117290860+urez06@users.noreply.github.com>
2023-02-02 20:59:53 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+import pytest
+
+from app.calc import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 1, 1) == 2
+
+    def test_zero_division_success(self):
+        with pytest.raises(ZeroDivisionError):
+                self.calc.division(self, 1, 0)
+
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_success(self):
+        assert self.calc.division(self, 6, 1) == 6
+
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 5, 7) == -2
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
